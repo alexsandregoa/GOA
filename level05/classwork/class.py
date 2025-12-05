673a9070001c7dbda5b5654b4f2e08e4dfc43860
alexsandregoa <gitprojectsgit@gmail.com>
2025-12-05 18:42:28 +0400

    1

---
+++ level05/classwork/class.py
@@ -0,0 +1,3 @@
+
+user_year=int(input('enteryou birthday year '))
+print('you are', 2025- user_year)
---
+++ level08/classwork/class.py
@@ -0,0 +1,12 @@
+for i in range(10):
+    print("alex")
+print(                                   )
+#Control Flow განმარტება: tells you რა რიგით უნდა გაკეთდეს კოდი
+
+#Control Flow-ის ტიპებია:
+#Sequencing – კოდი სრულდება ზემოდან ქვემოთ
+#Iteration – კოდის გამეორება
+#Selection – არჩევა თუ რომელი კოდი უნდა შესრულდეს
+#Algorithm – არის ნაბიჯ-ნაბიჯ გაწერილი ინსტრუქცია
+
+#Pseudocode – ალგორითმის გასაგებად დაწერილი კოდი
