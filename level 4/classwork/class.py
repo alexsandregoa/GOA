2a9036a68a0a14b1415cdd999c36e232dc40f3d2
alexsandregoa <gitprojectsgit@gmail.com>
2025-12-05 18:38:29 +0400

    1

---
+++ level 4/classwork/class.py
@@ -0,0 +1,6 @@
+#მომხარებელს შემოატანინეთ სახელი და შეინახეთ ცვლადში ussr_name, შემდეგ შემოატანინეთ ქალაქი, და შეინახეთ ცვლადში user_city
+
+#შემდეგ დაპრინტეთ წინადადება "გამარჯობა {user_name}, შენ ცხოვრობ {user_city} ში"  
+#Ussr_name=input("WWHAT YOUR NAME GANGY ")
+#
+##rint("GREAT"+"Ussr_name")
