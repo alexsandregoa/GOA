52e475dbe7b7efa649ea8908427db6188842f259
alexsandregoa <gitprojectsgit@gmail.com>
2025-12-05 19:00:02 +0400

    1

--- level 7/homework/home.py
+++ level 7/homework/home.py
@@ -1,54 +1,19 @@
-print(5 == 5)     # True
-print(5 != 3)     # True
-print(7 > 2)      # True
-print(3 < 8)      # True
-print(4 >= 4)     # True
-print(2 <= 5)     # True
-print(             )
-# Create two variables
-a = 10
-b = 7
+# and -> and ოპერატორობაში ორი ოპერატ მოკლე მაგალითად ორივე პირობა არის True (მართალია) და მაშინ გამოიტანს True 
+# False მაშინ გამომიტანს False მაგალითად
 
-# Compare the variables
-print("a == b:", a == b)   # Equal to
-print("a != b:", a != b)   # Not equal to
-print("a > b:", a > b)     # Greater than
-print("a < b:", a < b)     # Less than
-print("a >= b:", a >= b)   # Greater than or equal to
-print("a <= b:", a <= b)   # Less than or equal to
-print(                     )
-# Equal to
-print(5 == 5)      # True
+print(True and True)  # True
+print(True and False) # False
+print(False and False) # False
+print(False and True) # False
 
-# Not equal to
-print(5 != 3)      # True
+# or -> or ოპერატორის შემთხვევაში თუ ერთი პირობა მაინც არის True (მართალი) და იქედან გამოიტანს True,
+# თუ კი ორივე პირობა არიყვს False (მცდარი) მაშინ იმ შემთხვევაშიც გამოიტანს False ს
 
-# Greater than
-print(10 > 7)      # True
-
-# Less than
-print(2 < 9)       # True
-
-# Greater than or equal to
-print(8 >= 8)      # True
-
-# Less than or equal to
-print(4 <= 6)      # True
-print(             )
-import random
-
-# Create 2 variables with random numbers
-a = random.randint(1, 100)
-b = random.randint(1, 100)
-
-print("a =", a)
-print("b =", b)
-
-# Compare the variables
-print("a == b:", a == b)
-print("a != b:", a != b)
-print("a > b:", a > b)
-print("a < b:", a < b)
-print("a >= b:", a >= b)
-print("a <= b:", a <= b)
-print(                       )
+print(True or True)   # True
+print(True or False)  # True
+print(False or False) # False
+print(False or True) # True
+print(                                  )
+print(15>3 or 15<3)#or აღნიშნავს ან მაგალიტად იგივეა რაც მე გარეთ არ გავალ თუ არ იწვიმებს ან არ იქნება სეტყვა
+print(                                               )
+print(15>4 or 15 <9 and 12 > 3 or 0<19)#ტერმილალში გამოვა True
