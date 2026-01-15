7ac6e74cf2701eb932756e464d060367e465bdad
alexsandregoa <gitprojectsgit@gmail.com>
2026-01-15 23:34:11 +0400

    1

---
+++ level 13/homework/home.py
@@ -0,0 +1,19 @@
+cars = ["BMW", "Audi", "Toyota", "Mercedes", "Honda", "Ford", "Chevrolet", "Nissan", "Volkswagen", "Hyundai"]
+index = int(input("შეიყვანე რიცხვი (0-დან 9-მდე): "))
+cars[index] = "new element"
+print(cars)
+print(                                          )
+cars2 = ["BMW", "Audi", "Toyota", "Mercedes", "Honda", "Ford", "Chevrolet", "Nissan", "Volkswagen", "Hyundai", "Kia", "Mazda", "Subaru", "Dodge", "Jeep", "Lexus", "porshe", "Ferrari", "Lamborghini", "Bentley"]
+print(cars2[-1])
+print(cars2[-20])
+print(                                          )
+cars3 = ["BMW", "Audi", "Toyota", "Mercedes", "Honda", "Ford", "Chevrolet", "Nissan", "Volkswagen", "Hyundai"]
+index = int(input("შეიყვანე რიცხვი (0-დან 9-მდე): "))
+if index > 5:
+    print(cars3[index])
+else:
+    print("error")
+print(                                          )
+number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50]
+print (0,1,2,3,4,5,6,7,8,9)
+print (40,41,42,43,44,45,46,47,48,49)
