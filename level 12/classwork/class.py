b71849f24396322f99034ee80949ec6cbd163757
alexsandregoa <gitprojectsgit@gmail.com>
2026-01-15 22:55:29 +0400

    1

---
+++ level 12/classwork/class.py
@@ -0,0 +1,12 @@
+arr=[1,2,3,4,5,6,7,8,9,10]
+print(6 + 10)
+print(                                          )
+arr2 = [1,2,3,4,5,6,7,8,9,10]
+input_number = int(input("Enter a number: "))
+if input_number in arr2:
+    print(arr2[input_number ])
+print(                                          )
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+index = int(input("შეიყვანე რიცხვი (0-დან 9-მდე): "))
+numbers[index] = "new element"
+print(numbers)
