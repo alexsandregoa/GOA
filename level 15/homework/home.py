ce72542a1722be301354143ddf84c1a5da3a47d3
alexsandregoa <gitprojectsgit@gmail.com>
2026-02-01 19:12:37 +0400

    1

--- level 15/homework/home.py
+++ level 15/homework/home.py
@@ -1,14 +1,14 @@
 number = [1,2,3,4,5,6,7,8,9,10]
-numbers = number[int(1):int(5)]
+print(number[(1):(4)])
 print(                  )
 colors = ['red', 'blue', 'green', 'yellow', 'purple', 'orange']
-colors = colors[int(1):int(3)]
+print(colors[(1):(3)])
 print(                           )
 animals = ['cat', 'dog', 'fox', 'wolf', 'bear', 'lion']
-animals = animals[int(1):int(4)]
+print(animals[(1):(4)])
 print(                                  )
 letters = ['x', 'y', 'z', 'a', 'b', 'c', 'd']
-letters = letters[int(2):int(5)]
+print(letters[(2):(5)])
 print(                                )
 days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']
-days= days[int(1):int(4)]
+print(days[(1):(4)])
