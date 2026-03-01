fad42e0ac9427959352e2bd866a514b504dc345a
alexsandregoa <gitprojectsgit@gmail.com>
2026-03-01 15:47:56 +0400

    1

---
+++ level 19/homework/home.py
@@ -0,0 +1,36 @@
+# 1) მომხარებელს შემოატანინე სტრინგი. გადაიყვანე ყველა ასო დიდ ასოებად და დაბეჭდე.
+text = input("Enter some text: ")
+print(text.upper())
+
+# 2) მომხარებელს შემოატანინე სტრინგი. გადაიყვანე ყველა ასო პატარა ასოებად.
+text = input("Enter some text: ")
+print(text.lower())
+
+# 3) მომხარებელს შემოატანინე სტრინგი და პირველი ასო დიდად გადააქციე.
+text = input("Enter some text: ")
+print(text.capitalize())
+
+# 4) მომხარებელს შემოატანინე სტრინგი. ყველა სიტყვის პირველი ასო გაადიდე.
+text = input("Enter some text: ")
+print(text.title())
+print(                               )
+text = input("Enter some text: ")
+print(text.lower())
+print(                   )
+text = input("Enter some text: ")
+if text.isupper():
+    print("string doesnt have @")
+else:
+    print("your string have @ ")
+print(                                 )
+text = ("enter any book name: ")
+print(text.upper())
+print(                      )
+letter = input("Enter some letter: ")
+print(text[index])
+print(                                     )
+text = input("Enter some text: ")
+if text.isupper():
+    print("string is uppercase")
+else:
+    print("string is lowercase")
